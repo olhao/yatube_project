b7ccedb9411d216847ee5d598a414821aa306489
Olha <orukshyna@gmail.com>
2021-07-18 12:47:06 +0200

    Added models, admin

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'posts'
+
 urlpatterns = [
     # Главная страница
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts)
+    path('', views.index, name='index'),
+    path('group/', views.group_posts, name='group_posts')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,17 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Главная страница приложения Posts')
+    template = 'posts/index.html'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
 
-def group_posts(request, slug):
-    return HttpResponse(f'Группа по имени {slug}')
+def group_posts(request):
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
