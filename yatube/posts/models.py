78db1d42a148d98aa0c77224f1f0d3ef94f949dd
Olha <orukshyna@gmail.com>
2021-07-18 21:37:18 +0200

    Added urls, modified models, urls

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
@@ -12,3 +20,8 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True)
--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,5 +7,5 @@ app_name = 'posts'
 urlpatterns = [
     # Главная страница
     path('', views.index, name='index'),
-    path('group/', views.group_posts, name='group_posts')
+    path('group/<slug>/', views.group_posts, name='group_list')
 ]
